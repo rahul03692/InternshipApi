c2c6c2b43dc85fd1ab624f71ad8b84afbed2f510
rahul03692 <rahul03692@gmail.com>
2022-01-21 00:02:30 +0530

    cors added

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.enable("trust proxy");
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
+app,use(cors());
 //app.use("/api", routes);
 
 app.set('view engine','ejs');
